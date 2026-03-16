33d027f5071655a8686a93b2e6f4e3b89f09f4be
pavelmomo <kandrushin2003@yandex.ru>
2026-03-17 01:09:16 +0300

    refactor: add filter and pagination in GET events API

--- backend/app/event/event_router.py
+++ backend/app/event/event_router.py
@@ -1,11 +1,23 @@
-from fastapi import APIRouter
+from typing import Annotated
 
-from event.event_schemas import EventListDTO
+from fastapi import APIRouter, Depends
+from fastapi_filter.base.filter import FilterDepends
+
+from event.event_filter import EventFilter
+from event.event_schemas import EventShortDTO
 from event.event_service import EventService
+from utils.dependencies import UOW
+from utils.pagination import Page, PaginationParams
 
-event_router = APIRouter(prefix="/event", tags=["Event"])
+event_router = APIRouter(prefix="/events", tags=["Event"])
 
 
-@event_router.get("", response_model=EventListDTO, summary="Get events list")
-async def get_events():
-    return await EventService.get_events()
+@event_router.get(
+    "", response_model=Page[EventShortDTO], summary="Get actual events list"
+)
+async def get_actual_events(
+    uow: UOW,
+    filter: Annotated[EventFilter, FilterDepends(EventFilter)],
+    pagination_params: Annotated[PaginationParams, Depends()],
+):
+    return await EventService.get_actual_events(uow, filter, pagination_params)
